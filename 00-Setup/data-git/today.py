cd50775554eeda29a0706091cf5bac7b571c0ce9
Pablo Pussell <pp085ster@gmail.com>
2025-01-20 16:52:42 +0100

    Implemented my_name_is & my_buddy_is functions

---
+++ 00-Setup/data-git/today.py
@@ -0,0 +1,13 @@
+'''
+This is the module docstring
+'''
+
+def my_name_is():
+    '''returns my name'''
+
+    return 'pablopussell'
+
+def my_buddy_is():
+    '''returns name of buddy'''
+
+    return 'pablopussell'
