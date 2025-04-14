ba05cf02e03d3b648581ffd3d4dacef6e8b0952a
Pablo Pussell <pp085ster@gmail.com>
2025-04-14 17:05:05 +0200

    Implement my_name_is function

--- 00-Setup/data-git/today.py
+++ 00-Setup/data-git/today.py
@@ -2,12 +2,7 @@
 This is the module docstring
 '''
 
+
 def my_name_is():
-    '''returns my name'''
-
-    return 'pablopussell'
-
-def my_buddy_is():
-    '''returns name of buddy'''
-
-    return 'pablopussell'
+    github_nickname = "pablopussell"
+    return github_nickname
