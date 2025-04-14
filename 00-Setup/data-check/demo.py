6f10c2e528975a0543fbf0327fd55621dd6db045
Pablo Pussell <pp085ster@gmail.com>
2025-04-14 16:58:15 +0200

    Solved the first challenge of the bootcamp :)

--- 00-Setup/data-check/demo.py
+++ 00-Setup/data-check/demo.py
@@ -1,8 +1,9 @@
 """Dummy challenge for Kitt Demo"""
 
 
+
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
-    if radius > 0:
-        return 3.14*radius**2
-    return 0
+    if radius < 0:
+        return 0
+    return 3.14 * radius ** 2
