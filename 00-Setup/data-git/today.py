b1547a332eb477e985f96d9cc18fef64dbbde209
Pablo Pussell <pp085ster@gmail.com>
2025-04-15 11:42:11 +0200

    finished day 1

--- 00-Setup/data-git/today.py
+++ 00-Setup/data-git/today.py
@@ -4,9 +4,15 @@ This is the module docstring
 
 
 def my_name_is():
+    """
+    This function returns my name
+    """
     github_nickname = "pablopussell"
     return github_nickname
 
 def my_buddy_is():
+    """
+    This function returns my byddy's name
+    """
     buddy = "pablopussell"
     return buddy
